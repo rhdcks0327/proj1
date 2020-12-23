f3980fa36122c18950461b733f7b4b984dd02778
rhdcks0327 <76501258+rhdcks0327@users.noreply.github.com>
2020-12-23 16:00:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(function(){
-
+    $('#left a').each(function(){
+        var find_img = $(this).find('img');
+        var src_off = find_img.attr('src');  images/lastoff1_off.jpg
+        var src_on = src_off.replace('_off','_on')   //src_off 안에 있는 off --> on 바꿈
+                                             //images/lastoff1_on.jpg
+      $(this).hover(function(){
+        find_img.attr('src', src_on);
+      },function(){
+        find_img.attr('src', src_off);
+      })
+     })
     
     })
