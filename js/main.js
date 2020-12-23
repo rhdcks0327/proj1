9fd73d57db5229468de2bf4fa8509858428d8561
rhdcks0327 <76501258+rhdcks0327@users.noreply.github.com>
2020-12-23 22:11:30 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(function(){
-    $('#left a').each(function(){
-        var find_img = $(this).find('img');
-        var src_off = find_img.attr('src');  images/lastoff1_off.jpg
-        var src_on = src_off.replace('_off','_on')   //src_off 안에 있는 off --> on 바꿈
-                                             //images/lastoff1_on.jpg
-      $(this).hover(function(){
-        find_img.attr('src', src_on);
-      },function(){
-        find_img.attr('src', src_off);
-      })
-     })
-    
+  $('#top_popup > button').click(function(){
+    $('#top_popup').hide();
+  });
+  $(function(){
+    var slide;
+    slide = setInterval(function(){
+        $('#sec2 .notice > div:first-child').animate({
+          'margin-top':'-41px'},900,function(){
+              $('#sec2 .notice').append($('#sec2 .notice > div:nth-child(1)'));
+              $('#sec2 .notice > div:nth-child(3)').css(
+                                  {'margin-top':'41px'})
+          });
+        },1800)
+    });
+    $(function(){
+      var slide;
+      slide = setInterval(function(){
+          $('#sec2 .purp > div:first-child').animate({
+            'margin-top':'-41px'},900,function(){
+                $('#sec2 .purp').append($('#sec2 .purp > div:nth-child(1)'));
+                $('#sec2 .purp > div:nth-child(3)').css(
+                                    {'margin-top':'41px'})
+            });
+          },1800)
+      });
     })
